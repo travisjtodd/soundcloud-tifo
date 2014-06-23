065bf87b368035b8ed2d758a6779b374cb9ed055
Travis Todd <travisjtodd@me.com>
2014-06-23 20:24:59 +0200

    create teams

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+teams = [
+  {name: 'Algeria', tag: 'ALG'},
+  {name: 'Australia', tag: 'AUS'},
+  {name: 'Argentina', tag: 'ARG'},
+  {name: 'Belgium', tag: 'BEL'},
+  {name: 'Bosnia and Herzegovina', tag: 'BOS'},
+  {name: 'Brazil', tag: 'BRA'},
+  {name: 'Cameroon', tag: 'CMR'},
+  {name: 'Chile', tag: 'CHI'},
+  {name: 'Colombia', tag: 'COL'},
+  {name: 'Costa Rica', tag: 'CRC'},
+  {name: 'Côte d\'Ivoire', tag: 'CIV'},
+  {name: 'Croatia', tag: 'CRO'},
+  {name: 'Ecuador', tag: 'ECU'},
+  {name: 'England', tag: 'ENG'},
+  {name: 'France', tag: 'FRA'},
+  {name: 'Germany', tag: 'GER'},
+  {name: 'Ghana', tag: 'GHA'},
+  {name: 'Greece', tag: 'GRE'},
+  {name: 'Honduras', tag: 'HON'},
+  {name: 'Italy', tag: 'ITA'},
+  {name: 'Iran', tag: 'IRN'},
+  {name: 'Japan', tag: 'JPN'},
+  {name: 'Korea Republic', tag: 'KOR'},
+  {name: 'Mexico', tag: 'MEX'},
+  {name: 'Netherlands', tag: 'NED'},
+  {name: 'Nigeria', tag: 'NIG'},
+  {name: 'Portugal', tag: 'POR'},
+  {name: 'Russia', tag: 'RUS'},
+  {name: 'Spain', tag: 'ESP'},
+  {name: 'Switzerland', tag: 'SWI'},
+  {name: 'Uruguay', tag: 'URU'},
+  {name: 'USA', tag: 'USA'}
+]
+Team.create(teams)
