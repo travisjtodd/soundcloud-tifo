a78c08f6f902bd745269e4cbb289674093f3eb1f
Travis Todd <travisjtodd@me.com>
2014-06-26 09:26:53 +0200

    Maybe i COULD spell CLOUD correctly

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :soundcloud, ENV['SOUNDCOULD_KEY'], ENV['SOUNDCOULD_SECRET'], {:client_options => {:ssl => {:ca_path => "/usr/local/etc/openssl/"}}}
+  provider :soundcloud, ENV['SOUNDCLOUD_KEY'], ENV['SOUNDCLOUD_SECRET'], {:client_options => {:ssl => {:ca_path => "/usr/local/etc/openssl/"}}}
 end
